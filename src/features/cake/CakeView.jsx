91da87388626af37d2dc17d233c488a67c2b9c53
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-02-03 12:49:27 -0500

    useSelector

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux'
+
 export default function CakeView() {
+
+  const numOfCakes = useSelector((state) => state.cake.numOfCakes);
+  
   return (
     <article>
-        <h2>Number of Cakes - </h2>
+        <h2>Number of Cakes - {numOfCakes}</h2>
         <button>Order cake</button>
         <button>Restocke cakes</button>
     </article>
--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux'
+
 export default function IcecreamView() {
+
+  const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
+  
   return (
     <article>
-        <h2>Number of ice creams - </h2>
+        <h2>Number of ice creams - { numOfIcecreams }</h2>
         <button>Order ice creams</button>
         <button>Restocke ice creams</button>
     </article>
